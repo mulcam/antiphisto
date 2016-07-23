cdee16bc43bbf7772680ed65359bbc196c428836
doolee <doolee@doolee-PC>
2016-07-23 16:04:02 +0900

    Constants

--- SFS/src/kr/ac/konkuk/islab/Constants.java
+++ SFS/src/kr/ac/konkuk/islab/Constants.java
@@ -1,7 +1,7 @@
 package kr.ac.konkuk.islab;
 
 public class Constants {
-	final static String targetName = "credit"; //샘플네임
+	final static String targetName = "credit "; //샘플네임
 	final static String resultFolder = targetName + "_result"; //Weka파일 저장 위치
 	final static String dataFolder = targetName + "_data"; //Data파일 저장 위치
 	public static final int featureNum = 24; //특징 개수
